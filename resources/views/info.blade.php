ba6e4946d89ee08cdf3faeac19e7868e4e1cc5dc
NguyenNgocVan279 <nguyenngocvan279@gmail.com>
2021-04-13 00:59:20 +0700

    First commit

---
+++ resources/views/info.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('page_description')
+@section('page_title', 'About this project')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Info</div>
+
+                <div class="card-body">
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
